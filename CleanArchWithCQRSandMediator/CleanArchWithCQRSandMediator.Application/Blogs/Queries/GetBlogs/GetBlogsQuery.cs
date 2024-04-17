230f41fa58708cc08a4437497ad0d8a108fc7043
ANPHATPLASTIC\cntt.ptda3 <minhhieu2032000@gmail.com>
2024-04-17 16:58:43 +0700

    upload code

---
+++ CleanArchWithCQRSandMediator/CleanArchWithCQRSandMediator.Application/Blogs/Queries/GetBlogs/GetBlogsQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchWithCQRSandMediator.Application.Blogs.Queries.GetBlogs
+{
+    public class GetBlogsQuery: IRequest<List<BlogVm>>
+    {
+
+    }
+}
---
+++ CleanArchWithCQRSandMediator/CleanArchWithCQRSandMediator.Application/Blogs/Queries/GetBlogs/GetBlogsQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CleanArchWithCQRSandMediator.Domain.Repository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchWithCQRSandMediator.Application.Blogs.Queries.GetBlogs
+{
+    public class GetBlogsQueryHandler : IRequestHandler<GetBlogsQuery, List<BlogVm>>
+    {
+        private readonly IBlogRepository _blogRepository;
+        private readonly IMapper _mapper;
+
+        public GetBlogsQueryHandler(IBlogRepository blogRepository, IMapper mapper)
+        {
+            _blogRepository = blogRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<BlogVm>> Handle(GetBlogsQuery request, CancellationToken cancellationToken)
+        {
+            var blogs = await _blogRepository.GetAllAsync();
+
+            return _mapper.Map<List<BlogVm>>(blogs);
+        }
+    }
+}
