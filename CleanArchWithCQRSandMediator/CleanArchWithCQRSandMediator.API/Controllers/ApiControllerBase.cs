22f6220e9f23bab2f26c6bd98d53330c0a082a74
ANPHATPLASTIC\cntt.ptda3 <minhhieu2032000@gmail.com>
2024-04-17 17:08:44 +0700

    upload code

--- CleanArchWithCQRSandMediator/CleanArchWithCQRSandMediator.API/Controllers/ApiControllerBase.cs
+++ CleanArchWithCQRSandMediator/CleanArchWithCQRSandMediator.API/Controllers/ApiControllerBase.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchWithCQRSandMediator.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ApiControllerBase : ControllerBase
+    public abstract class ApiControllerBase : ControllerBase
     {
+        private ISender _mediator;
+
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
     }
 }
--- CleanArchWithCQRSandMediator/CleanArchWithCQRSandMediator.API/Controllers/BlogController.cs
+++ CleanArchWithCQRSandMediator/CleanArchWithCQRSandMediator.API/Controllers/BlogController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CleanArchWithCQRSandMediator.Application.Blogs.Queries.GetBlogs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchWithCQRSandMediator.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BlogController : ControllerBase
+    public class BlogController : ApiControllerBase
     {
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var blogs = await Mediator.Send(new GetBlogsQuery());
+            return Ok(blogs);
+        }
     }
 }
